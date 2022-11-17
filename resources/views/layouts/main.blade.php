f4693dbbd632ef075cceb52a1ad67783b85c2b0c
Matheus Passos <msena789@gmail.com>
2022-11-17 08:40:50 -0300

    Criando layout com blade + font family + bootstrap

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonte do Google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;600;700;900&display=swap" rel="stylesheet">
+
+        <!-- CSS Boostrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body class="antialiased">
+        @yield('content')
+        <footer>
+            <p>HDC Events &copy; 2022</p>
+        </footer>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'Home | HDC Events')
 
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body class="antialiased">
-        <h1>Algum titulo</h1>
-        @if(10 > 5)
-            <p>A condição é true</p>
-        @endif
+@section('content')
 
-        @if($nome == "Pedro")
-            <p>O nome é Pedro</p>
-        @elseif($nome == 'Matheus')
-            <p>O nome é {{ $nome }} e a idade é {{ $idade }}</p>
-        @else
-            <p>O nome não é Pedro nem Matheus</p>
-        @endif
+<h1>Algum titulo</h1>
+@if(10 > 5)
+    <p>A condição é true</p>
+@endif
+@if($nome == "Pedro")
+    <p>O nome é Pedro</p>
+@elseif($nome == 'Matheus')
+    <p>O nome é {{ $nome }} e a idade é {{ $idade }}</p>
+@else
+    <p>O nome não é Pedro nem Matheus</p>
+@endif
+@for($i = 0; $i < count($arr); $i++)
+    <p>{{ $arr[$i] }} - {{ $i }}</p>
+@endfor
+@php
+    $name = 'João';
+    echo $name;
+@endphp
+<!-- Comentário no HTML -->
+{{-- Comentário no Blade --}}
+@foreach($nomes as $nome)
+    <p>{{ $loop->index }}</p>
+    <p>{{ $nome }}</p>
+@endforeach
 
-        @for($i = 0; $i < count($arr); $i++)
-            <p>{{ $arr[$i] }} - {{ $i }}</p>
-        @endfor
-
-        @php
-            $name = 'João';
-            echo $name;
-        @endphp
-
-        <!-- Comentário no HTML -->
-        {{-- Comentário no Blade --}}
-
-        @foreach($nomes as $nome)
-            <p>{{ $loop->index }}</p>
-            <p>{{ $nome }}</p>
-        @endforeach
-    </body>
-</html>
+@endsection
